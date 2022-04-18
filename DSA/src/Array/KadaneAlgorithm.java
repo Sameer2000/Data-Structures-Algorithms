6b96ccc9e44fed06384156a719746df64de038f3
samee <samee@LAPTOP-L49ROB4H>
2022-04-18 21:23:56 +0530

    Kadane's Algorithm

---
+++ DSA/src/Array/KadaneAlgorithm.java
@@ -0,0 +1,27 @@
+// find Largest sum contiguous Subarray [V. IMP]
+
+package Array;
+
+public class KadaneAlgorithm {
+	
+	public static long kadaneAlgorithm(int arr[], int n) {
+		int max = Integer.MIN_VALUE;
+		int subSum = 0;
+		for(int i = 0; i < n; i++) {
+			subSum += arr[i];
+			max = Math.max(max, subSum);
+			if(subSum < 0) {
+				subSum = 0;
+			}
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		System.out.println(kadaneAlgorithm(arr, arr.length));
+		
+	}
+
+}
