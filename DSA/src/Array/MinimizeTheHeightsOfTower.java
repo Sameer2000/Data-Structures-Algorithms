12108ec3d5c6d0910e41300a1a79ffe1f702dcf8
samee <samee@LAPTOP-L49ROB4H>
2022-04-25 12:09:10 +0530

    Minimize the difference.

--- DSA/src/Array/MinimizeTheHeightsOfTower.java
+++ DSA/src/Array/MinimizeTheHeightsOfTower.java
@@ -2,26 +2,36 @@
 
 package Array;
 
+import java.util.Arrays;
+
 public class MinimizeTheHeightsOfTower {
 	
-	public static int minimizeHeight(int[] arr, int n, int k) {
-		int max, min;
-		int result = arr[n-1] - arr[0];
-		
-		for(int i = 1; i < n; i++) {
-			if(arr[i] - k >= 0) {
-				max = Math.max(arr[i-1] + k, arr[n-1] - k);
-				min = Math.min(arr[0] + k, arr[i] - k);
-			    result = Math.min(result, max - min);
-			}
-		}
-		return result;
+	public static int minimizeHeight(int[] A, int K) {
+		int n = A.length;
+        Arrays.sort(A);
+        int ans = A[n - 1] - A[0];
+        int minimum = A[0] + K;
+        int maximum = A[n - 1] - K;
+        int current_minimum = 0, current_maximum = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            // Finding the minimum and the maximum value if we perform the decrement operation on all the
+            // indexes greater than index i and increment operation until the index i.
+            current_minimum = Math.min(A[i + 1] - K, minimum);
+            current_maximum = Math.max(A[i] + K, maximum);
+            // Checking if minimum value is non-negative or not.
+            if (current_minimum >= 0)
+            {
+                ans = Math.min(ans, current_maximum - current_minimum);
+            }
+        }
+        return ans;
 	}
 
 	public static void main(String[] args) {
 		
 		int arr[] = {3,9,12,16,20};
-		System.out.println(minimizeHeight(arr, arr.length, 3));
+		System.out.println(minimizeHeight(arr, 3));
 
 	}
 
